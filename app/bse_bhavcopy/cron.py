6571e9f1aaad08695a01386dc1915876f0484bbe
Rituraj Jain <rituraj.jain17@gmail.com>
2021-02-08 21:53:01 +0530

    Reformat cron.py using autopep8 and add comments

--- app/bse_bhavcopy/cron.py
+++ app/bse_bhavcopy/cron.py
@@ -12,14 +12,21 @@ from django.conf import settings
 
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
 
+# User Agent header is required as bseindia.com
+# returns 403 error when downloading csv data
 headers = {'User-Agent': user_agent}
 
 required_fields = ('SC_CODE', 'SC_NAME', 'OPEN', 'HIGH', 'LOW', 'CLOSE')
 
 redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
-                                  port=settings.REDIS_PORT, db=0)
+                                   port=settings.REDIS_PORT, db=0)
+
 
 def download_bhavcopy():
+    """
+    This function runs as a cron.
+    It downloads the equity data from yesterday and writes it to the redis db.
+    """
     yesterday_date = (datetime.today() - timedelta(days=1)).strftime('%d%m%y')
     response = requests.get(
         f'https://www.bseindia.com/download/BhavCopy/Equity/EQ{yesterday_date}_CSV.ZIP', headers=headers
